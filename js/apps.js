505fceb8ac26154de7edcd000b01514e855b80ff
Mikal Morello <mikalmorello@gmail.com>
2016-01-12 23:12:21 -0500

    Add card layout functionality, add project role and skill, add header test branding, add header scroll css class change

--- js/apps.js
+++ js/apps.js
@@ -135,5 +135,20 @@ app.controller('ctrl', function ($scope) {
     };
 });
 
+// SCROLL TEST
 
+app.directive("scroll", function ($window) {
+    return function(scope, element, attrs) {
+        angular.element($window).bind("scroll", function() {
+             if (this.pageYOffset >= 100) {
+                 scope.boolChangeClass = true;
+                 console.log('Scrolled below header.');
+             } else {
+                 scope.boolChangeClass = false;
+                 console.log('Header is in view.');
+             }
+            scope.$apply();
+        });
+    };
+});
 
