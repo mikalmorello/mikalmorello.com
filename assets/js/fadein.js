218cec9434b40668f63d72ea523b765c6254b85b
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-02-19 21:41:40 -0500

    JS - Add section fade in and scroll to functionality

---
+++ assets/js/fadein.js
@@ -0,0 +1,35 @@
+/* FADE IN CONTENT */
+
+(function(){
+  
+var animateHTML = function() {
+  var elems;
+  var windowHeight;
+  function init() {
+    elems = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+    addEventHandlers();
+    checkPosition();
+  }
+  function addEventHandlers() {
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  }
+  function checkPosition() {
+    for (var i = 0; i < elems.length; i++) {
+      var positionFromTop = elems[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        elems[i].className = elems[i].className.replace(
+          'hidden',
+          'fade-in-element'
+        );
+      }
+    }
+  }
+  return {
+    init: init
+  };
+};
+animateHTML().init();
+
+})();
