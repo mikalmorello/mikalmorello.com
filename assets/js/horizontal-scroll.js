23951a1dde1bd0498e841fa8b5a942bf87a44379
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-02-28 18:21:25 -0500

    THEME - Clean up horizontal scroll code

--- assets/js/horizontal-scroll.js
+++ assets/js/horizontal-scroll.js
@@ -2,7 +2,7 @@
   
   // VARIABLES
   
-  const slider = document.querySelector('.items');
+  const slider = document.querySelector('.horizontal-scroll__items');
   let isDown = false;
   let startX;
   let scrollLeft;
@@ -11,17 +11,17 @@
   
   slider.addEventListener('mousedown', (e) => {
     isDown = true;
-    slider.classList.add('active');
+    slider.classList.add('horizontal-scroll__items--active');
     startX = e.pageX - slider.offsetLeft;
     scrollLeft = slider.scrollLeft;
   });
   slider.addEventListener('mouseleave', () => {
     isDown = false;
-    slider.classList.remove('active');
+    slider.classList.remove('horizontal-scroll__items--active');
   });
   slider.addEventListener('mouseup', () => {
     isDown = false;
-    slider.classList.remove('active');
+    slider.classList.remove('horizontal-scroll__items--active');
   });
   slider.addEventListener('mousemove', (e) => {
     if(!isDown) return;
