5750817b4e8968da05c1fa102fdde8c9d5c0a9d8
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-03-04 21:55:02 -0500

    THEME - Mockup, continue refactoring, remove information points

--- assets/js/mockup.js
+++ assets/js/mockup.js
@@ -1,39 +1,21 @@
 jQuery(document).ready(function($){
-	//open interest point description
-	$('.cd-single-point').children('a').on('click', function(){
-		var selectedPoint = $(this).parent('li');
-		if( selectedPoint.hasClass('is-open') ) {
-			selectedPoint.removeClass('is-open').addClass('visited');
-		} else {
-			selectedPoint.addClass('is-open').siblings('.cd-single-point.is-open').removeClass('is-open').addClass('visited');
-		}
-	});
-	//close interest point description
-	$('.cd-close-info').on('click', function(event){
-		event.preventDefault();
-		$(this).parents('.cd-single-point').eq(0).removeClass('is-open').addClass('visited');
-	});
-
+  
 	//on desktop, switch from product intro div to product mockup div
-	$('#cd-start').on('click', function(event){
+	$('#mockupButton').on('click', function(event){
 		event.preventDefault();
 		//detect the CSS media query using .cd-product-intro::before content value
-		var mq = window.getComputedStyle(document.querySelector('.cd-product-intro'), '::before').getPropertyValue('content');
-		if(mq == 'mobile') {
+		var mediaQuery = window.getComputedStyle(document.querySelector('.mockup__intro'), '::before').getPropertyValue('content');
+		if(mediaQuery == 'mobile') {
 			$('body,html').animate({'scrollTop': $($(this).attr('href')).offset().top }, 200); 
 		} else {
-			$('.cd-product').addClass('is-product-tour').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				$('.cd-close-product-tour').addClass('is-visible');
-				$('.cd-points-container').addClass('points-enlarged').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
-					$(this).addClass('points-pulsing');
-				});
+			$('.mockup__container').addClass('mockup__container--open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$('.mockup__close-button').addClass('is-visible');
 			});
 		}
 	});
 	//on desktop, switch from product mockup div to product intro div
-	$('.cd-close-product-tour').on('click', function(){
-		$('.cd-product').removeClass('is-product-tour');
-		$('.cd-close-product-tour').removeClass('is-visible');
-		$('.cd-points-container').removeClass('points-enlarged points-pulsing');
+	$('.mockup__close-button').on('click', function(){
+		$('.mockup__container').removeClass('mockup__container--open');
+		$('.mockup__close-button').removeClass('is-visible');
 	});
 });
