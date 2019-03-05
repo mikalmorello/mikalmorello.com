7ca34fb21dd84f665bee90c8d3951301bc9ce598
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-03-04 22:15:14 -0500

    JS - Remove JQuery dependence on mockup

--- assets/js/mockup.js
+++ assets/js/mockup.js
@@ -1,21 +1,36 @@
-jQuery(document).ready(function($){
+/* MOCKUP */
+
+(function(){
   
-	//on desktop, switch from product intro div to product mockup div
-	$('#mockupButton').on('click', function(event){
-		event.preventDefault();
-		//detect the CSS media query using .cd-product-intro::before content value
-		var mediaQuery = window.getComputedStyle(document.querySelector('.mockup__intro'), '::before').getPropertyValue('content');
-		if(mediaQuery == 'mobile') {
-			$('body,html').animate({'scrollTop': $($(this).attr('href')).offset().top }, 200); 
-		} else {
-			$('.mockup__container').addClass('mockup__container--open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				$('.mockup__close-button').addClass('is-visible');
-			});
-		}
-	});
-	//on desktop, switch from product mockup div to product intro div
-	$('.mockup__close-button').on('click', function(){
-		$('.mockup__container').removeClass('mockup__container--open');
-		$('.mockup__close-button').removeClass('is-visible');
-	});
-});
+  // VARIABLES
+  
+  const mockupCloseButton = document.getElementById('mockupCloseButton'),
+        mockupContainer = document.getElementById('mockupContainer'),
+        mockupButton = document.getElementById('mockupButton');
+  
+  
+  // FUNCTION
+  
+  function showOverlay(callback){
+    mockupContainer.classList.add('mockup__container--open');
+    setTimeout(callback);
+  }
+  
+  function showCloseButton(){
+    mockupCloseButton.classList.add('is-visible');
+  }
+  
+  
+  // CLICK EVENTS
+  
+  mockupButton.addEventListener('click', function() {
+    event.preventDefault();
+    showOverlay(showCloseButton);
+  });
+
+  mockupCloseButton.addEventListener('click', function() {
+    mockupContainer.classList.remove('mockup__container--open');
+    mockupCloseButton.classList.remove('is-visible');
+  });
+
+})();
