e13f89409c3ca9454642da77fe39ff8c6dde5808
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-03-07 22:31:29 -0500

    THEME - Homepage, contact, menu, initial mobile responsive updates

--- assets/js/main-menu.js
+++ assets/js/main-menu.js
@@ -5,6 +5,7 @@
   // VARIABLES
   
   const body = document.getElementsByTagName('body')[0],
+        header = document.getElementById('header'),
         headerMenuButton = document.getElementById('headerMenuButton'),
         mainMenu = document.getElementById('mainMenu');
 
@@ -12,12 +13,14 @@
   
   function showMenu(callback){
     mainMenu.classList.add('main-menu--is-visible');
+    header.classList.add('header--menu-light');
     body.classList.add('body--menu-open');
     setTimeout(callback, 400);
   }
   
   function hideMenu(callback){
     mainMenu.classList.remove('main-menu--is-visible');
+    header.classList.remove('header--menu-light');
     body.classList.remove('body--menu-open');
     callback();
   }
