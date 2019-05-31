65efc8c79706e8c972759e5b1fc952823aa9913d
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-05-30 21:15:20 -0400

    THEME - Add company header content

--- assets/js/company.js
+++ assets/js/company.js
@@ -2,23 +2,50 @@
 
 (function(){
   
-  //VARIABLES
-  const body = document.getElementsByTagName('body')[0];
+  // VARIABLES
   
-  let company = window.location.hash.substring(1);
+  const body = document.getElementsByTagName('body')[0],
+        logo = document.getElementsByClassName('header__brand')[0];
+  
+  let company = window.location.hash.substring(1),
+      companyDiv = document.createElement("div");
+  
+  
+  // FUNCTIONS
+  
+  function setCompany(companyName){
+    console.log(companyName + 'exists');
+    companyDiv.innerHTML =       
+        `
+          <div class="header__company">
+            <span class="header__company-sign">+ </span>
+            <img class="header__company-logo" src="/assets/images/company/edx.svg" alt="edx" />
+          </div>
+        `
+      ;
+    logo.appendChild(companyDiv);
+  }
+
   
   switch(company) {
     case 'harvard': 
       body.classList.add('body--harvard');
-      console.log('Harvard ' + company);
       break;
     case 'mit':
       body.classList.add('body--mit');
-      console.log('MIT ' + company);
+      break;
+    case 'edx':
+      body.classList.add('body--edx');
+      setCompany(company);
+      break;
+    case 'hbsp':
+      body.classList.add('body--hbsp');
+      break;
+    case 'wgbh':
+      body.classList.add('body--wgbh');
       break;
     case 'massart':
       body.classList.add('body--massart');
-      console.log('MassArt ' + company);
       break;
     default:
       break;
