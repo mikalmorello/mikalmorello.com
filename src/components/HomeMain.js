92f7c1ae2fe11f74e8358a59bc262598447e6dab
Mikal Morello <mikal_morello@harvard.edu>
2021-05-16 17:12:10 -0400

    Add Firebase

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import SvgCanon from './SvgCanon';
-import Fade from 'react-reveal/Fade';
-import { 
-  Link
-} from 'react-router-dom';
+import React from "react";
+import SvgCanon from "./SvgCanon";
+import Fade from "react-reveal/Fade";
+import { Link } from "react-router-dom";
 
 class HomeMain extends React.Component {
-  
-  render(){
+  render() {
     return (
       <main id="main" className="main">
         <section id="sectionIntroduction" className="section section--introduction">
@@ -15,10 +12,12 @@ class HomeMain extends React.Component {
             <div className="introduction">
               <div className="introduction__container">
                 <div className="introduction__text">
-                  <p>Currently the <span className="semi-bold">lead front-end web developer</span> at Tufts University, I am passionate about building scalable design systems that enhance the user-experience. From hands-on development to high-level strategic planning, I've done it all. Check out my latest work below:</p>
+                  <p>
+                    Currently a<span className="semi-bold">digital accessibility developer</span> at Harvard University, I am passionate about building scalable design systems that enhance the user-experience. From hands-on development to high-level strategic planning, I've done it all. Check out my latest work below:
+                  </p>
                 </div>
                 <div className="introduction__divider">
-                  <img src="/assets/images/wave-divider.svg" alt=""/>
+                  <img src="/assets/images/wave-divider.svg" alt="" />
                 </div>
               </div>
             </div>
@@ -31,7 +30,7 @@ class HomeMain extends React.Component {
                 <div className="project-teaser">
                   <div className="project-teaser__container">
                     <Link to="/tufts-magazine" className="project-teaser__content">
-                     <h3 className="project-teaser__title">
+                      <h3 className="project-teaser__title">
                         <span className="project-teaser__white-background">Tufts Magazine</span>
                       </h3>
                       <h4 className="project-teaser__subtitle">
@@ -39,16 +38,16 @@ class HomeMain extends React.Component {
                       </h4>
                     </Link>
                     <div className="project-teaser__media">
-                      <img src="/assets/images/project-tufts-magazine-thumbnail.png" alt="Tufts University Magazine screenshot" /> 
+                      <img src="/assets/images/project-tufts-magazine-thumbnail.png" alt="Tufts University Magazine screenshot" />
                     </div>
                   </div>
                 </div>
               </Fade>
               <Fade>
-                <div className="project-teaser"> 
+                <div className="project-teaser">
                   <div className="project-teaser__container">
                     <Link to="/tufts-university-web-standard" className="project-teaser__content">
-                     <h3 className="project-teaser__title">
+                      <h3 className="project-teaser__title">
                         <span className="project-teaser__white-background">Tufts Web Standard</span>
                       </h3>
                       <h4 className="project-teaser__subtitle">
@@ -56,8 +55,8 @@ class HomeMain extends React.Component {
                       </h4>
                     </Link>
                     <div className="project-teaser__media">
-                      <img src="/assets/images/project-tufts-standard-thumbnail.jpg" alt="Tufts D8 Standard screenshot"  /> 
-                    </div> 
+                      <img src="/assets/images/project-tufts-standard-thumbnail.jpg" alt="Tufts D8 Standard screenshot" />
+                    </div>
                   </div>
                 </div>
               </Fade>
@@ -65,7 +64,7 @@ class HomeMain extends React.Component {
                 <div className="project-teaser">
                   <div className="project-teaser__container">
                     <Link to="/boston-college-redesign" className="project-teaser__content">
-                     <h3 className="project-teaser__title">
+                      <h3 className="project-teaser__title">
                         <span className="project-teaser__white-background">Boston College Website Redesign</span>
                       </h3>
                       <h4 className="project-teaser__subtitle">
@@ -73,7 +72,7 @@ class HomeMain extends React.Component {
                       </h4>
                     </Link>
                     <div className="project-teaser__media">
-                      <img src="/assets/images/project-boston-college-thumbnail.jpg" alt="Boston College Redesign screenshot" /> 
+                      <img src="/assets/images/project-boston-college-thumbnail.jpg" alt="Boston College Redesign screenshot" />
                     </div>
                   </div>
                 </div>
@@ -81,7 +80,7 @@ class HomeMain extends React.Component {
             </div>
           </div>
         </section>
-        <Fade>  
+        <Fade>
           <section id="sectionContact" className="section section--contact">
             <div className="contact-card contact-card--full">
               <div className="contact-card__container">
@@ -99,11 +98,10 @@ class HomeMain extends React.Component {
               </div>
             </div>
           </section>
-        </Fade> 
+        </Fade>
       </main>
-    )
+    );
   }
-  
 }
 
 export default HomeMain;
